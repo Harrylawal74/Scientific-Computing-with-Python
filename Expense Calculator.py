49645624fef08d736af3ed989c5b6e4c3aa78cb5
Harry Lawal <103933304+Harrylawal74@users.noreply.github.com>
2024-09-28 21:24:56 +0100

    Update Expense Calculator.py
    Added comments.
    Finish Comments

--- Expense Calculator.py
+++ Expense Calculator.py
@@ -1,16 +1,32 @@
+#adds an expense to the 'expenses' list in main() function by using a dictionary
+#Takes parameters and adds them to a dictionary in the 'expenses' list defined in the main() function
 def add_expense(expenses, amount, category):
     expenses.append({'amount': amount, 'category': category})
     
+
+#Outputs out each expense and its corresponding amount
+#Takes 'expenses' as a perameter and iterates over it outputting out each expense and its corresponding amount
 def print_expenses(expenses):
     for expense in expenses:
         print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
     
+    
+#Outputs the sum of all expenses
+#Lambda function uses expense as an unknown and searches for 'amount' in whatever is specified in th map function )expenses)
+#Map function makes this ^ happen for as many iterations as there are expenses and stores values in a list (each amount in expenses is stored in a list)
+#Sum function adds up the values in the list
 def total_expenses(expenses):
     return sum(map(lambda expense: expense['amount'], expenses))
     
+    
+#Outputs total expense of specific category
+#Takes 'expenses' list and category as perameters 
+#Lambda function uses expense as an unknown and searches for 'category' in whatever is specified by the filter function (expenses)
+#Filter function makes this ^ happen for as many iterations as there are expenses and creates a True/False list according to the condition of the logic statement 
 def filter_expenses_by_category(expenses, category):
     return filter(lambda expense: expense['category'] == category, expenses)
     
+
 
 def main():
     expenses = []
